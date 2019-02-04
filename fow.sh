97047b1b79dec10e1a7294e434baed0de20d992a
Smarthard <th3smartchan@gmail.com>
2019-02-04 13:52:21 +0300

    added wrapping for JARs and raster images

--- fow.sh
+++ fow.sh
@@ -1,20 +1,44 @@
 #!/bin/sh
 
-editor() {
-	if [ "$EDITOR" = "" ]; then
-			echo "Environment virable EDITOR is unset"
+envvar() {
+	if [ -z ${!1} ]; then
+			echo "Environment virable ${1} is unset" >&2 
 			exit 1
 	fi
+}
+
+editor() {
+	envvar "EDITOR"
 	"$EDITOR" "$1"
+}
+
+rimage() {
+	envvar "RIMAGE"
+	"$RIMAGE" "$1"
 }
 
 
 for arg in $@; do
-	case `file "$arg"` in
+	ftype=`file "$arg"`
+
+	shopt -s nocasematch
+	case "$ftype" in
+		
+		### RASTER IMAGES
+		*PNG*|*GIF*|*JPEG*)
+				rimage "$arg" ;;
+
+		### JAVA ARCHIVE RUNNABLE	
+		*JAR*)
+				java -jar "$arg" ;;
+
+		### TEXT FILES (code sources like .c or .java included)
 		*text*) 
 				editor "$arg" ;;
+
+		### UNKNOWN FILES
 		*)
-				echo "Unknown file type. Please, spesify open mode" 
+				echo -e "$ftype\nUnknown file type. Please, spesify open mode" >&2 
 				exit 1 ;;
 	esac
 done
