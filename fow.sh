cc71fe04fd45e89ea89f64aa002226d5b6f50731
Smarthard <th3smartchan@gmail.com>
2019-02-07 14:32:18 +0300

    Parameter substitution instead envvar

--- fow.sh
+++ fow.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-envvar() {
-	if [ -z "${!1}" ]; then
-			echo "Environment virable ${1} is unset" >&2 
-			exit 1
-	fi
-}
-
 
 for arg in $@; do
 	ftype=`file "$arg"`
@@ -16,11 +9,11 @@ for arg in $@; do
 		
 		### RASTER IMAGES
 		*PNG*|*GIF*|*JPEG*)
-				envvar "FOWSH_RIMAGE" && "$FOWSH_RIMAGE" "$arg" ;;
+				${FOWSH_RIMAGE:?} "$arg" ;;
 
 		### VECTOR IMAGES
 		*SVG*)
-				envvar "FOWSH_VIMAGE" && "$FOWSH_VIMAGE" "$arg" ;;
+				${FOWSH_VIMAGE:?} "$arg" ;;
 
 		### JAVA ARCHIVE RUNNABLE	
 		*JAR*)
@@ -28,7 +21,7 @@ for arg in $@; do
 
 		### TEXT FILES (code sources like .c or .java included)
 		*text*) 
-				envvar "EDITOR" && "$EDITOR" "$arg" ;;
+				${EDITOR:?} "$arg" ;;
 
 		### UNKNOWN FILES
 		*)
