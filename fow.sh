fe8f7dec8f9cdeba2a48b7344b174ea9ebacbfb2
Smarthard <th3smartchan@gmail.com>
2019-04-11 15:38:39 +0300

    stdin args processing

--- fow.sh
+++ fow.sh
@@ -5,6 +5,62 @@ usage() {
 	exit 1
 }
 
+fow() {
+	ftype=`file "$1"`
+
+	shopt -s nocasematch
+	case "$ftype" in
+		
+		### RASTER IMAGES
+		*PNG*|*GIF*|*JPEG*)
+				${FOWSH_RIMAGE:?} "$1" ;;
+
+		### VECTOR IMAGES
+		*SVG*)
+				${FOWSH_VIMAGE:?} "$1" ;;
+
+		### JAVA ARCHIVE RUNNABLE	
+		*JAR*)
+				java -jar "$1" ;;
+
+		### TEXT FILES (code sources like .c or .java included)
+		*text*) 
+				${EDITOR:?} "$1" ;;
+
+		### VIDEO
+		*video*)
+				${FOWSH_VIDEO:?} "$1" ;;
+
+		### EMPTY FILES (not archives)
+		*empty)
+				;;
+
+		### UNKNOWN FILES
+		*)
+
+				### LINKS (URLs or domains)
+
+				ip="[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(:[0-9]+)?"
+				url="(http[s]?:\/\/)?[a-zA-Z0-9]+\.[a-zA-Z0-9]{2,}(:[0-9]+)?"
+				if [[ "$arg" =~ $ip|$url ]]; then
+					${BROWSER:?} "$1"
+				else
+					echo -e "$ftype\nUnknown file type. Please, spesify open mode" >&2 
+					exit 1
+				fi
+	esac
+}
+
+force() {
+	if [ ! -z "$o" ]; then
+		${o} "$arg"
+		
+		return 0
+	else
+		return 1
+	fi
+}
+
 while getopts "o:n:t:" arg; do
 	case "${arg}" in
 		o)
@@ -19,7 +75,7 @@ while getopts "o:n:t:" arg; do
 done
 shift $((OPTIND-1))
 
-if [ $# -eq 0 ]; then
+if [ -t 0 -a $# -eq 0 ]; then
 	usage
 fi
 
@@ -27,10 +83,13 @@ if [ ! -z "$n" -o ! -z "$t" ]; then
 	echo "options -n and -t not yet available"
 fi
 
+if read -t 0 ; then
+	while read arg; do
 
-for arg in "$@"; do
-	ftype=`file "$arg"`
-	
+		force "$arg" || fow "$arg"
+
+	done < /dev/stdin
+else
 	if [ $# -gt 10 ]; then
 		if [ -z "$proceed" ]; then
 			echo -n "fowsh is going to open $# files. Would you like to proceed? [y/N] "
@@ -43,51 +102,8 @@ for arg in "$@"; do
 		esac
 	fi
 
-	# forced opening -o
-	if [ ! -z "$o" ]; then
-		${o} "$arg"
-		continue
-	fi
+	for arg in "$@"; do
+		force "$arg" || fow "$arg"
+	done
+fi
 
-	shopt -s nocasematch
-	case "$ftype" in
-		
-		### RASTER IMAGES
-		*PNG*|*GIF*|*JPEG*)
-				${FOWSH_RIMAGE:?} "$arg" ;;
-
-		### VECTOR IMAGES
-		*SVG*)
-				${FOWSH_VIMAGE:?} "$arg" ;;
-
-		### JAVA ARCHIVE RUNNABLE	
-		*JAR*)
-				java -jar "$arg" ;;
-
-		### TEXT FILES (code sources like .c or .java included)
-		*text*) 
-				${EDITOR:?} "$arg" ;;
-
-		### VIDEO
-		*video*)
-				${FOWSH_VIDEO:?} "$arg" ;;
-
-		### EMPTY FILES (not archives)
-		*empty)
-				;;
-
-		### UNKNOWN FILES
-		*)
-
-				### LINKS (URLs or domains)
-
-				ip="[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(:[0-9]+)?"
-				url="(http[s]?:\/\/)?[a-zA-Z0-9]+\.[a-zA-Z0-9]{2,}(:[0-9]+)?"
-				if [[ "$arg" =~ $ip|$url ]]; then
-					${BROWSER:?} "$arg"
-				else
-					echo -e "$ftype\nUnknown file type. Please, spesify open mode" >&2 
-					exit 1
-				fi
-	esac
-done
